e30a5ba9dc97508095ae6b1d91da30703ba0875a
Dyoseus <albert_h_le@hotmail.com>
2023-07-24 15:59:04 -0400

    AHHH

--- message.py
+++ message.py
@@ -2,11 +2,18 @@ import socket
 import datetime
 import client
 
+
+now = datetime.datetime.now()
 class Message:
     def __init__(self, sender: str, payload: str, timestamp: datetime.datetime, chatroom_id: str, uuid: str):
         self.sender = sender  
-        self.payload = payload
-        self.timestamp = timestamp
+
+        if payload is None:
+            Exception("Message can't be blank")
+        else:
+            self.payload = payload
+        self.timestamp = now
+        
         self.chatroom_id = chatroom_id
         self.uuid = uuid
 
