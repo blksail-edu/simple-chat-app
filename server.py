c3d25bea1d0c7073bd32b35c48a181d32348e0e1
ES0227 <erinsuh0227@gmail.com>
2023-07-24 21:57:24 -0400

    fixed a few things

--- server.py
+++ server.py
@@ -1,14 +1,15 @@
 from pymavlink import mavutil
 import socket as skt
 import unittest
-class SERVER:
+class Server:
     """you input your server Ip and server port number,
     and the progran will check for validity for both input"""
-def __init__(self, serverIp, serverPrt):
-        self.serverPrt = int(serverPrt)
+def __init__(self, serverIp: str, serverPrt: int):
+        self.serverPrt = serverPrt
         if serverPrt != range(6000,49000):
             raise ValueError
         self.serverPrt = serverPrt
+        self.serverIp = serverIp
 """this tests for the validity of ip address by using the inet_aton to convert the ip address to a 32 bit
 data, if it can successfully convert """
 def validate_ip_address(self, serverIp):
@@ -24,4 +25,4 @@ if __name__ == "__main__":
 
 #trials
 def test_serverPrt(self):
-    self.assertTrue(SERVER.validate_ip_address("10.29.33.207"), "10.29.33.207")
+    self.assertTrue(Server.validate_ip_address("10.29.33.207"), "10.29.33.207")
